54a7b3e8bb7c52d04743a681f6c9a9599e22f446
Łukasz Narloch <lukassz111@gmail.com>
2020-11-03 15:51:30 +0100

    x

---
+++ zadanie3/src/services/MovieService.js
@@ -0,0 +1,43 @@
+import { BehaviorSubject } from 'rxjs'
+import { RequestService } from './RequestService'
+
+class _MovieService {
+  baseUrl = './wikipedia-movie-data/'
+  movies = []
+  movieToDisplay = []
+  movieToDisplayObservable = new BehaviorSubject()
+  page = 0
+  perPage = 10
+
+  getLastPageNumber () {
+    let m = this.movies.length % this.perPage
+    return (this.movies.length - m) / this.perPage
+  }
+
+  listUpdate () {
+    if (this.page < 0) {
+      this.page = 0
+    } else if (this.page > this.getLastPageNumber()) {
+      this.page = this.getLastPageNumber()
+    }
+
+    this.movieToDisplay = []
+    let begin = this.page * this.perPage
+    let end = begin + this.perPage
+    for (let i = begin; i < end; i++) {
+      this.movieToDisplay.push(this.movies[i])
+    }
+    this.movieToDisplayObservable.next()
+  }
+
+  loadMovies () {
+    let url = this.baseUrl + 'movies.json'
+    RequestService.get(url).then((response) => {
+      this.movies = JSON.parse(response)
+      this.page = 0
+      this.listUpdate()
+    })
+  }
+}
+const MovieService = new _MovieService()
+export { MovieService }
---
+++ zadanie3/src/views/MovieListPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="movieListPage">
+    <MovieList/>
+  </div>
+</template>
+
+<script>
+import MovieList from '../components/MovieList.vue'
+
+export default {
+  name: 'MovieListPage',
+  components: {
+    MovieList
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../scss/theme.scss';
+@import '../scss/functions.scss';
+#movieListPage {
+  width: 100%;
+  margin: 0 auto;
+}
+</style>
